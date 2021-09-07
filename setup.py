1b5539972a64552265d73cc9b1c09c22d0631872
brandon-sexton <brandon.sexton.1@outlook.com>
2021-09-06 18:11:13 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+	name="pip_template",
+	version="0.1.0",
+	description="Template for building pip packages",
+	long_description=long_description,
+	author="Brandon Sexton",
+	author_email="brandon.sexton.1@outlook.com",
+	packages=find_packages()
+	)

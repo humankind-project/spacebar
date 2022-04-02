3172529b05033d4083cebf9d2753e623d0a6831b
brandon-sexton <brandon.sexton.1@outlook.com>
2022-04-01 18:08:18 -0600

    initial commit

--- spacebar/astro/bodies.py
+++ spacebar/astro/bodies.py
@@ -1,9 +1,9 @@
 class Earth:
-    """
-    Class used to represent Earth.  Values are defined using EGM96 geopotential
-    model.  Constant naming convention is intentionally not used since the 
-    values defined here may be updated in the future by introducing other
-    geopotential models
+    """Class used to represent Earth.  
+    
+    Values are defined using EGM96 geopotential model.  Constant naming 
+    convention is intentionally not used since the values defined here may be 
+    updated in the future by introducing other geopotential models
     """
     mu = 3.986004415e5 #km^3/s^2
     equatorial_radius = 6378.1363 #km
--- spacebar/math/linalg.py
+++ spacebar/math/linalg.py
@@ -1,25 +1,33 @@
-from audioop import mul
 from math import sqrt
 
 class Vector3D:
 
     def __init__(self, x:float, y:float, z:float) -> None:
-        """
-        Class used to represent three-dimensional vectors and perform basic 
-        linear algebra.  This class will most often be used to represent a 
-        position or velocity
-        """
+        """three-dimensional vector used for basic linear algebra
 
+        Args:
+            x:      The first component of the vector
+            y:      The second component of the vector
+            z:      The third component of the vector
+
+        Returns:
+            None
+
+        """
         self.x = x
         self.y = y
         self.z = z
 
     def plus(self, vec_to_add:"Vector3D") -> "Vector3D":
-        """
-        Method used to add the elements of the calling vector to the argument
-        vector
-        """
+        """Performs vector addition
 
+        Args:       
+            vec_to_add:     The vector which will be added to the calling vector
+
+        Returns:
+            AVector representing element addition of self and vec_to_add
+
+        """
         return Vector3D(
             self.x + vec_to_add.x, 
             self.y + vec_to_add.y,
@@ -27,32 +35,43 @@ class Vector3D:
         )
 
     def minus(self, vec_to_subtract:"Vector3D") -> "Vector3D":
-        """
-        Method used to subtract the elements of the argument vector from the
-        elements of the calling vector
-        """
+        """Performs vector subtraction
 
+        Args:
+            vec_to_subtract:    The vector which will be subtracted from self
+
+        Returns:
+            Vector representing element difference of self and vec_to_subtract
+
+        """
         return Vector3D(
             self.x - vec_to_subtract.x, 
             self.y - vec_to_subtract.y,
             self.z - vec_to_subtract.z
         )
 
-    def dot(self, vec_to_dot:"Vector3D") -> "Vector3D":
-        """
-        Method used to get the dot product of the calling vector and the 
-        argument vector
-        """
+    def dot(self, vec_to_dot:"Vector3D") -> float:
+        """Performs dot product
 
+        Args:
+            vec_to_dot:     The vector to be used in the product with self
+
+        Returns:
+            scalar value representing sum of element multiplication
+
+        """
         return self.x*vec_to_dot.x + self.y*vec_to_dot.y + self.z*vec_to_dot.z
 
     def cross(self, vec_to_cross:"Vector3D") -> "Vector3D":
-        """
-        Method used to get the cross product of the calling vector and the
-        argument vector.  Order matters here, the format is self.cross(vec)
-        is equivalent to self X vec
-        """
+        """Performs cross product
 
+        Args:
+            vec_to_cross:   The vector to be used in the product with self
+
+        Returns:
+            Vector perpendicular to self and vec_to_cross 
+
+        """
         return Vector3D(
             self.y*vec_to_cross.z - self.z*vec_to_cross.y,
             self.z*vec_to_cross.x - self.x*vec_to_cross.z,
@@ -60,21 +79,38 @@ class Vector3D:
         )
 
     def normalize(self) -> "Vector3D":
-        """
-        Method used to get the unit vector for the calling vector
+        """Method used to get the unit vector for the calling vector
+
+        Args:
+            None
+
+        Returns:
+            Vector parallel to self and of length 1
+
         """
         mag = self.magnitude()
         return Vector3D(self.x/mag, self.y/mag, self.z/mag)
         
 
     def magnitude(self) -> float:
-        """
-        Method used to get the length of the calling vector
+        """Method used to get the length of the calling vector
+
+        Args:
+            None
+
+        Returns:
+            Square root of the sum of squared components
+
         """
         return sqrt(self.x**2 + self.y**2 + self.z**2)
 
     def scale(self, multiple:float) -> "Vector3D":
-        """
-        Method used to scale each element by the specified multiple
+        """Method used to scale each element by the specified multiple
+
+        Args:
+            multiple:   value to be multiplied across elements
+
+        Returns:
+            Vector parallel to self with a scaled magnitude 
         """
         return Vector3D(self.x*multiple, self.y*multiple, self.z*multiple)
